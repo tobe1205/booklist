21ce9921e5b2116be9c4d2e0a7e63e5b1c509dd1
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-03-07 22:48:33 +0900

    booklist - 도서 목록 관리 프로그램

---
+++ src/com/kh/project/list/library/run/Run.java
@@ -0,0 +1,13 @@
+package com.kh.project.list.library.run;
+
+
+import com.kh.project.list.library.view.BookMenu;
+
+public class Run {
+
+
+    public static void main(String[] args) {
+        new BookMenu();
+
+    }
+}
